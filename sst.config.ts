16552bb5fe066179a5906d38fad349e2ee5522d2
sintariran <k0127s@gmail.com>
2025-04-07 10:40:36 +0900

    Adding storage

--- sst.config.ts
+++ sst.config.ts
@@ -4,17 +4,14 @@ export default $config({
   app(input) {
     return {
       name: "monorepo-template",
+      region: "ap-northeast-1", // ここが加わる
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
   async run() {
-    const storage = await import("./infra/storage");
+    await import("./infra/storage");
     await import("./infra/api");
-
-    return {
-      MyBucket: storage.bucket.name,
-    };
   },
 });
